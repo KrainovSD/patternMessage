ced6d398b0a32be74bee9db2c25a758b69ed8bc1
KrainovSD <denislosev48@gmail.com>
2022-07-06 15:57:18 +0300

    add act to load users info for tokens

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,22 @@
     <div class="userInfo">
       <h1>Bret</h1>
       <div class="userInfo__container">
-        <button class="userInfo__loadButton">
+        <button class="userInfo__loadButton" @click="loadNewRandomUser">
           Загрузить случайного пользователя
         </button>
         <p class="userInfo__title">Имя пользователя</p>
-        <input type="text" class="userInfo_inputInfo" />
+        <input readonly type="text" class="userInfo_inputInfo" :value="name" />
         <p class="userInfo__title">Адресс e-mail</p>
-        <input type="text" class="userInfo_inputInfo" />
+        <input readonly type="text" class="userInfo_inputInfo" :value="email" />
         <p class="userInfo__title">Контактный телефон</p>
-        <input type="text" class="userInfo_inputInfo" />
+        <input
+          readonly
+          type="text"
+          class="userInfo_inputInfo"
+          :value="number"
+        />
         <p class="userInfo__title">Основной веб-сайт</p>
-        <input type="text" class="userInfo_inputInfo" />
+        <input readonly type="text" class="userInfo_inputInfo" :value="site" />
       </div>
     </div>
     <div class="editor">
@@ -27,8 +32,7 @@
       <textarea
         name=""
         id=""
-        cols="30"
-        rows="10"
+        maxlength="264"
         class="editor__editField"
       ></textarea>
       <div class="editor__storageTools">
@@ -41,9 +45,9 @@
       </div>
       <textarea
         name=""
+        readonly
         id=""
-        cols="30"
-        rows="10"
+        maxlength="264"
         class="editor__patternField"
       ></textarea>
     </div>
@@ -51,8 +55,65 @@
 </template>
 
 <script>
+function random(max) {
+  return Math.floor(Math.random() * max);
+}
 export default {
   name: "App",
+  userData: {
+    Delphine: {
+      name: "Glenna Richert",
+      email: "Chaim_McDermott@dana.io",
+      number: "(775)976-6974 x41206",
+      site: "condrad.com",
+    },
+    Leopoldo_Corkery: {
+      name: "Mrs. Dennis Schullst",
+      email: "Karley_Dach@jasper.info",
+      number: "1-477-935-8478 x6430",
+      site: "ola.org",
+    },
+    Bret: {
+      name: "Leanne Graham",
+      email: "Sincere@april.biz",
+      number: "1-770-736-8031 x56442",
+      site: "hidegard.org",
+    },
+    "Elwyn.Skilles": {
+      name: "Kurtis Welssnat",
+      email: "Telly.Hoeger@billy.biz",
+      number: "210.067.6132",
+      site: "condrad.com",
+    },
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+      number: "",
+      site: "",
+      editField: "",
+    };
+  },
+  created() {
+    this.loadNewRandomUser();
+  },
+  methods: {
+    loadNewRandomUser() {
+      let keyList = Object.keys(this.$options.userData);
+      let randomID = random(keyList.length);
+      let randomUser = Object.fromEntries(
+        Object.entries(this.$options.userData[keyList[randomID]])
+      );
+      if (this.name == randomUser.name) {
+        this.loadNewRandomUser();
+        return;
+      }
+      Object.keys(randomUser).forEach((el) => {
+        this[el] = randomUser[el];
+      });
+    },
+  },
 };
 </script>
 
